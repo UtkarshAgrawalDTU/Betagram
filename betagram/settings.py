1f253cc2486c9fbc84a251c2cefb8418fc5b86bf
utkarsh <agrawal.utkarsh8@gmail.com>
2020-01-05 22:09:16 +0530

    STATIC FILES MOD.

--- betagram/settings.py
+++ betagram/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'

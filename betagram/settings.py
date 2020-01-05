7e8116118626110020a7d509190d812526d7f21e
utkarsh <agrawal.utkarsh8@gmail.com>
2020-01-05 22:34:05 +0530

    Mod

--- betagram/settings.py
+++ betagram/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATIC_URL = '/static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'

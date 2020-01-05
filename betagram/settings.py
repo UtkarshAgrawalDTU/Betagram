65e9f1f305e2b9efb5c5a4a8c7a50c88c8da9a85
utkarsh <agrawal.utkarsh8@gmail.com>
2020-01-06 01:39:18 +0530

    Added django-heroku

--- betagram/settings.py
+++ betagram/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'crispy_forms',
     'storages',
+    'django_heroku',
 ]
 
 MIDDLEWARE = [
@@ -159,3 +160,5 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())

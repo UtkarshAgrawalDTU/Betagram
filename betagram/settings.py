71af526225b54adebd9e1a40090d9fafc0372923
utkarsh <agrawal.utkarsh8@gmail.com>
2020-01-06 02:10:40 +0530

    Hojaa

--- betagram/settings.py
+++ betagram/settings.py
@@ -160,5 +160,9 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_REGION_NAME = "ap-south-1"
+AWS_S3_SIGNATURE_VERSION = "s3v4"
+
+
 
 django_heroku.settings(locals())
